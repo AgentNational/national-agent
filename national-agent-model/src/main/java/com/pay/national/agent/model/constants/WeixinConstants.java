2f6722b1092cc12b4c68b009b7ebe1dcd0e0918e
liuzhenhui <m17600695056@163.com>
2018-01-16 19:09:28 +0800

    微信accessToken管理

--- national-agent-model/src/main/java/com/pay/national/agent/model/constants/WeixinConstants.java
+++ national-agent-model/src/main/java/com/pay/national/agent/model/constants/WeixinConstants.java
@@ -5,5 +5,9 @@ public class WeixinConstants {
 	/** 永久二维码 **/
 	public static final String qrcodeUrl = "https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=";// 永久二维码
 
+	/**
+	 * 获取访问微信接口的token地址
+	 */
+	public static final String tokenUrl = "https://api.weixin.qq.com/cgi-bin/token";
 
 }
