8c6dc8e022c901c6dbd270fe93b3a2a6d85ce72d
liuzhenhui <m17600695056@163.com>
2018-02-03 15:56:21 +0800

    配置微信支付相关

--- national-agent-commons/src/main/java/com/pay/national/agent/common/constants/PayConstants.java
+++ national-agent-commons/src/main/java/com/pay/national/agent/common/constants/PayConstants.java
@@ -6,7 +6,7 @@ public class PayConstants {
     public static final String mchid = "1496028552";// 全民代理微信支付商户号
     public static final String mch_appid = "wx9e242df43f4eb301";// 全民代理微信支付appid
     public static final String appsecret = "7bedbf2b2ca2c380e5a891096764f9a7";// 全民代理微信支付appsecret
-    public static final String paysign = "cdf506985f7a4d298aa32916631ac614";// 全民代理微信支付秘钥
+    public static final String paysign = "cfd576985f7a4d298aa32916631ac614";// 全民代理微信支付秘钥
 
 
 }
